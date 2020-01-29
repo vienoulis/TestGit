e806a4447846578925c7e37a4a1112ef5fa548ca
Лукьянов Роман Андреевич <vienoulis@gmail.com>
2020-01-29 03:54:03 +0300

    Новая папка, txt
    Добавил пару не нужных файлов

--- Main.java
+++ Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello GitHub!!!");
+        for (int i = 0; i < 100; i++) {
+            System.out.println("Cheng");
+        }
     }
 }
