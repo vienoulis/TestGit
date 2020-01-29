d8ecd0713feb8f0d20f151100150d4b7d346de2e
Лукьянов Роман Андреевич <vienoulis@gmail.com>
2020-01-29 04:10:55 +0300

    Действие1.0
    Я не понял что сделал

--- Main.java
+++ Main.java
@@ -4,8 +4,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello GitHub!!!");
-        for (int i = 0; i < 100; i++) {
-            System.out.println("Cheng");
-        }
     }
 }
